ec0af910151bc47b8760e5d4289bacf259b1d5e3
Tairone <tairone.alves@gmail.com>
2023-10-03 18:53:38 -0300

    031023

---
+++ capitulo7/slice.js
@@ -0,0 +1,6 @@
+// slice() do objeto String, ele gera uma nova variavel com o conteudo extraido da string
+
+let s = 'JavaScript é uma linguagem legal';
+
+console.log(s.slice(5, -5)) // O segundo paramentro indica até onde vai a extração, se o numero for negativo, contará no inverso
+
---
+++ capitulo7/split.js
@@ -0,0 +1,6 @@
+// split(), divide uma string no formato de array sendo o divisor passado como paramentro.
+
+
+let s = 'JavaScript é uma linguagem legal';
+
+console.log(s.split(' ')) // Array(5) [ "JavaScript", "é", "uma", "linguagem", "legal" ]
