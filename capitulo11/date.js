ebc751a07150886b791bc3bf160b8fb0fbed22c0
Tairone <tairone.alves@gmail.com>
2023-10-11 19:12:01 -0300

    111023_1911

---
+++ capitulo11/date.js
@@ -0,0 +1,8 @@
+let d = new Date(); // data e hora atual de acordo com o pc
+console.log('Hora atual sem parametro =>', d); 
+
+let a  = new Date(26786958);
+console.log('Hora com parametro milissegundos =>', a);
+
+let b = new Date('07/26/1987 10:59:59');
+console.log('Data no formato string =>', b);
---
+++ capitulo11/exemplo1.js
@@ -0,0 +1,25 @@
+// Getdate()
+
+let a = new Date(1987, 6, 26, 10, 11, 12, 586);
+console.log('Data setada =>', a);
+
+console.log('getDate exibe o dia entre 01 e 31 =>', a.getDate());
+
+console.log('getDay, retorna o dia da semana =>', a.getDay());
+
+console.log('getFullYear, retorna o ano no formato de 4 digitos =>', a.getFullYear());
+
+console.log('getHours, retorna horas de 0 a 23 =>', a.getHours());
+
+console.log('getMilleseconds, retorna os milissegundos setados no Date() =>', a.getMilliseconds());
+
+console.log('getMinutes, retorna minutos 0-59 =>', a.getMinutes());
+
+console.log('getMouth, retona o número dos meses de 0-11 =>', a.getMonth());
+
+console.log('getSeconds, retona o número de segundos 0-59 =>', a.getSeconds());
+
+console.log('getime, retona o número de milissegundos desde 01/01/1960 =>', a.getTime());
+
+
+
