0254e2b10e8a3a8c9406cf2b1e326ea786745377
Tairone <tairone.alves@gmail.com>
2023-11-16 12:29:41 -0300

    161123_1229

---
+++ capitulo12/exemplo1.js
@@ -0,0 +1,7 @@
+// setInterval, executa a mesma ação a cada intervalo, em um loop infinito
+
+// setInterval(() => {
+// console.log('Passram 4 segundos');
+// }, 4000)
+
+console.log('Não esperei')
